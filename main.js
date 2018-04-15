7f9a125cc8af394f8844c16e674e1a7e89fe8548
Darin Estrin <darin.estrin@gmail.com>
2018-04-15 14:45:34 -0700

    user can add a streamer to a list of streamers and the list will be saved to local storage for later use

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-  
+  var streamers = new Streamers();
+
+  $('.add-streamers').on('submit', function(e) {
+    e.preventDefault();
+    streamers.addStreamer(e.target[0].value);
+    e.target[0].value = '';
+  });
 });
+
+// Twitch Streamers object
+function Streamers(streamers) {
+  this.streamers = [];
+
+  /**
+   * 
+   * @param {string} streamer 
+   * adds a streamer to the users list of streamers
+   */
+  this.addStreamer = function(streamer) {
+    if (this.streamers.indexOf(streamer) > 0) {
+      return;
+    }
+
+    this.streamers.push(streamer);
+    localStorage.setItem('streamers', JSON.stringify(this.streamers));
+  }
+}
